ba8c2bad58072cce0f9186d11d63915cb33e28c7
Martin Richard <richard.efio@gpsinka.com>
2018-09-10 17:29:20 -0500

    Sistema Gerencial Version 1.1

--- routes/web.php
+++ routes/web.php
@@ -107,6 +107,7 @@ Route::middleware(['auth'])->group(function(){
     Route::post('/compra/importFileForum', 'ExcelController@importFileForum');
     Route::post('/compra/readFileForum', 'ExcelController@readFileForum');
     Route::post('/compra/SetForum', 'CompraController@SetForum');
+    Route::get('/gescotizacion/GetListCotizacionesByIdVendedor', 'CotizacionController@GetListCotizacionesByIdVendedor');
 
 
     Route::get('/gescontacto/UpdReasignarContacto', 'GestionContactoController@UpdReasignarContacto');
