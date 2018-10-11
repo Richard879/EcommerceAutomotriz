69dbc59ee4d9be9614148384aee307f1a1f75bf6
Richard Efio Rivas <richard.efio@gpsinka.com>
2018-10-11 09:03:17 -0500

    Sistema Gerencial Version 1.0

--- resources/assets/js/components/PdiProcesoInspeccion.vue
+++ resources/assets/js/components/PdiProcesoInspeccion.vue
@@ -2031,29 +2031,29 @@
     }
 </script>
 <style>
-        .mostrar{
-            display: list-item !important;
-            opacity: 1 !important;
-            position: fixed !important;
-            background-color: #3c29297a !important;
-            overflow-y: scroll;
-        }
-        .modal-content{
-            width: 100% !important;
-            position: absolute !important;
-        }
-        .error{
-            display: flex;
-            justify-content: center;
-        }
-        .text-center{
-            color: red;
-            font-weight: bold;
-            font-size: 0.75rem;
-        }
-        .disabled{
-            opacity: 0.65;
-            cursor: not-allowed;
-            pointer-events:none;
-        }
+    .mostrar{
+        display: list-item !important;
+        opacity: 1 !important;
+        position: fixed !important;
+        background-color: #3c29297a !important;
+        overflow-y: scroll;
+    }
+    .modal-content{
+        width: 100% !important;
+        position: absolute !important;
+    }
+    .error{
+        display: flex;
+        justify-content: center;
+    }
+    .text-center{
+        color: red;
+        font-weight: bold;
+        font-size: 0.75rem;
+    }
+    .disabled{
+        opacity: 0.65;
+        cursor: not-allowed;
+        pointer-events:none;
+    }
 </style>
