ee80f16506386ec1c277ccb68c291ee0ae344328
Juan Manuel Anton <juanma.03.17.07@gmail.com>
2018-11-26 18:24:41 -0500

    Prueba API

--- app/Http/Controllers/Api/ComprasController.php
+++ app/Http/Controllers/Api/ComprasController.php
@@ -8,32 +8,49 @@ use App\Http\Controllers\Controller;
 
 class ComprasController extends Controller
 {
-    public function GetListCompraByVin($id){
+    public function GetListCompraByVin()
+    {
         $client = new Client([
             // Base URI se utiliza con solicitudes relativas
-            'base_uri' => 'http:/172.20.6.62:53218/SrvCompra.svc?WSDL/',// URI Ejem: https://jsonplaceholder.typicode.com
+            'base_uri' => 'http://192.168.114.1:53218/SrvCompra.svc?WSDL',
         ]);
 
-        // Enviar una solicitud del verbo GET a http://192.168.114.1:53218/SrvCompra.svc/List_GetListCompraByVin?id=MALC281CAJM305579
-        $response = $client->request('GET', "/List_GetListCompraByVin/{$id}");
+        $response = $client->request('GET', "/List_GetListCompraByVin");
 
         return $response;
-        // Solo si la rpta es JSON se debe ingresar al contenido del cuerpo y decodificar
-        // $posts = json_decode($response->getBody()->getContents());
-        // return $posts;
     }
 
-    public function getPost(){
+    /*
+        // public function GetListCompraByVin($id){
+        //     $client = new Client([
+        //         // Base URI se utiliza con solicitudes relativas
+        //         'base_uri' => 'http:/172.20.6.62:53218/SrvCompra.svc?WSDL/',// URI Ejem: https://jsonplaceholder.typicode.com
+        //     ]);
+
+        //     // Enviar una solicitud del verbo GET a http://192.168.114.1:53218/SrvCompra.svc/List_GetListCompraByVin?id=MALC281CAJM305579
+        //     $response = $client->request('GET', "/List_GetListCompraByVin/{$id}");
+
+        //     return $response;
+        //     // Solo si la rpta es JSON se debe ingresar al contenido del cuerpo y decodificar
+        //     // $posts = json_decode($response->getBody()->getContents());
+        //     // return $posts;
+        // }
+    */
+
+    public function GetPosts($id)
+    {
         $client = new Client([
             // Base URI se utiliza con solicitudes relativas
             'base_uri' => 'https://jsonplaceholder.typicode.com',
         ]);
 
-        // Enviar una solicitud del verbo GET a https://jsonplaceholder.typicode.com/posts
-        $response = $client->request('GET', "/posts");
+        // Enviar una solicitud del verbo GET a https://jsonplaceholder.typicode.com/posts/1
+        $response = $client->request('GET', "posts/{$id}");
 
         // Solo si la rpta es JSON se debe ingresar al contenido del cuerpo y decodificar
-        $posts = json_decode($response->getBody()->getContents());
+        // $posts = json_decode($response->getBody()->getContents());
+        $posts = ($response->getBody()->getContents());
         return $posts;
+        //dd($posts);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -308,7 +308,7 @@ Route::middleware(['auth'])->group(function(){
     Route::get('/gescotizacion/GetListObsequiosByVehiculo', 'CotizacionController@GetListObsequiosByVehiculo');
     Route::get('/gescotizacion/GetListObsequioElementoVenta', 'CotizacionController@GetListObsequioElementoVenta');
 
-    Route::get('/getComprasApi/GetListCompraByVin/{id}', 'Api\ComprasController@GetListCompraByVin');
+    Route::get('/getComprasApi/GetListCompraByVin', 'Api\ComprasController@GetListCompraByVin');
     //Prueba con api que retorna JSON
-    Route::get('/getComprasApi/getPost', 'Api\ComprasController@getPost');
+    Route::get('getComprasApi/GetPosts/{id}', 'Api\ComprasController@GetPosts');
 });
