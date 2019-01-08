660f95a3bcabf5d6ee8e10d11ebdfd4200d87340
Juan Manuel Anton <juanma.03.17.07@gmail.com>
2019-01-08 15:46:27 -0500

    Integración en Generación Descuentos en los Pedidos

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -77,7 +77,7 @@ const app = new Vue({
     el: '#app',
     data: {
         menu: 1300362,
-        ruta: 'http://172.20.6.54:8080/saisacsys/public'
+        ruta: 'http://172.20.6.51:8080/saisacsys/public'
     },
     methods: {
         reiniciaMenu: function(data) {
