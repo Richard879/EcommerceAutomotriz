08a74e594f5082a1f557521df2533a669a68a7f8
Juan Manuel Anton <juanma.03.17.07@gmail.com>
2019-01-23 16:29:53 -0500

    Generar Entrada Mercancia

--- resources/assets/js/components/Compra.vue
+++ resources/assets/js/components/Compra.vue
@@ -2092,6 +2092,7 @@
                         me.registroSapArticulo();
                     }
                     else{
+                        me.loadingProgressBar("OCURRIO UN PROBLEMA...");
                         me.verResultados();
                     }
                 }).catch(error => {
@@ -2145,19 +2146,24 @@
                 me.arraySapArticulo.map(function(x, y){
                     // Si no se encuentra
                     //[01]
-                    if (!me.arraySapItemCode.includes(x.cNumeroVin)) {
+                    // if (!me.arraySapItemCode.includes(x.cNumeroVin)) {
+                    //     me.arraySapCompra.push(x);
+                    // }
+                    //Si el VIN del arraySapArticulo se encuentra en arraySapItemCode => guardar en compra
+                    //Sino se encuentra no pase a compra
+                    if (me.arraySapItemCode.includes(x.cNumeroVin)) {
                         me.arraySapCompra.push(x);
                     }
                 });
 
                 var sapUrl = me.ruta + '/compra/SapSetCompra';
                 axios.post(sapUrl, {
-                    'cCardCode': me.formCompra.ccarcode,
-                    'fDocDate': moment().format('YYYY-MM-DD'),
-                    'fDocDueDate': moment().add(30, 'days').format('YYYY-MM-DD'),
-                    'WarehouseCode': me.formCompra.warehousecode,
-                    'Igv': 1 + parseFloat((me.formCompra.igv)),
-                    'data': me.arraySapCompra
+                    'cCardCode'     :   me.formCompra.ccarcode,
+                    'fDocDate'      :   moment().format('YYYY-MM-DD'),
+                    'fDocDueDate'   :   moment().add(30, 'days').format('YYYY-MM-DD'),
+                    'WarehouseCode' :   me.formCompra.warehousecode,
+                    'Igv'           :   1 + parseFloat((me.formCompra.igv)),
+                    'data'          :   me.arraySapCompra
                 }).then(response => {
                     me.arraySapRptCompra = response.data;
                     me.arraySapRptCompra.map(function(x){
