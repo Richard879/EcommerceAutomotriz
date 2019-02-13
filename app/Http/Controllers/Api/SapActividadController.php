8a9acbda1190f26bcdb6e92743ee9f6c00e1a100
Richard Efio Rivas <richard.efio@gpsinka.com>
2019-02-13 16:54:25 -0500

    Sap Integracion Version 1.0

--- app/Http/Controllers/Api/SapActividadController.php
+++ app/Http/Controllers/Api/SapActividadController.php
@@ -17,6 +17,46 @@ class SapActividadController extends Controller
             'base_uri'  => 'http://172.20.0.10/'
         ]);
 
+        $array_rpta = [];
+        $rptaSap   = [];
+
+        $data = $request->data;
+        foreach ($data as $key => $value) {
+
+            $json = [
+                'json' => [
+                    "ActivityDate"      => (string)$value['dActivityDate'],
+                    "ActivityTime"      => $value['hActivityTime'],
+                    "CardCode"          => $value['cCardCode'],
+                    "DocEntry"          => $value['nDocEntry'],
+                    "DocNum"            => $value['nDocNum'],
+                    "DocType"           => $value['nDocType'],
+                    "Duration"          => $value['nDuration'],
+                    "DurationType"      => $value['cDurationType'],
+                    "EndDueDate"        => (string)$value['dEndDueDate'],
+                    "EndTime"           => $value['hEndTime'],
+                    "Reminder"          => $value['cReminder'],
+                    "ReminderPeriod"    => $value['nReminderPeriod'],
+                    "ReminderType"      => $value['cReminderType'],
+                    "StartDate"         => (string)$value['dStartDate'],
+                    "StartTime"         => $value['hStartTime']
+                ]
+            ];
+
+            $response = $client->request('POST', "/api/Actividad/SapSetActividad/", $json);
+            $rptaSap = json_decode($response->getBody());
+            array_push($array_rpta, $rptaSap);
+        }
+        return $array_rpta;
+    }
+
+    public function SapSetActividadVenta(Request $request)
+    {
+        $client = new Client([
+            'verify'    => false,
+            'base_uri'  => 'http://172.20.0.10/'
+        ]);
+
         // ======================================================================
         // GENERAR ACTIVIDAD PARA EL VEHICULO
         // ======================================================================
