400039152d497ff6aa7abc2a1ed6ec46945de2c2
Richard Efio Rivas <richard.efio@gpsinka.com>
2019-03-21 09:47:23 -0500

    Sap Integracion Version 1.0

--- app/Http/Controllers/Api/SapLlamadaServicioController.php
+++ app/Http/Controllers/Api/SapLlamadaServicioController.php
@@ -128,4 +128,37 @@ class SapLlamadaServicioController extends Controller
         ];
     }
 
+    public function SapSetSolucion(Request $request)
+    {
+        $client = new Client([
+            'verify'    => false,
+            'base_uri'  => 'http://172.20.0.10/'
+        ]);
+
+        $array_rpta = [];
+        $rptaSap   = [];
+
+        $data = $request->data;
+        foreach ($data as $key => $value) {
+
+            $json = [
+                'json' => [
+                    "CustomerCode"         => $value['cCustomerCode'],
+                    "InternalSerialNum"    => $value['cInternalSerialNum'],
+                    "ItemCode"             => $value['cItemCode'],
+                    "Subject"              => $value['cSubject'],
+                    "ServiceCallActivities" => [
+                        [
+                            "ActivityCode"=> (string)$value['nActivityCode']
+                        ]
+                    ]
+                ]
+            ];
+
+            $response = $client->request('POST', "/api/LlamadaServicio/SapSetLlamadaServicio/", $json);
+            $rptaSap = json_decode($response->getBody());
+            array_push($array_rpta, $rptaSap);
+        }
+        return $array_rpta;
+    }
 }
