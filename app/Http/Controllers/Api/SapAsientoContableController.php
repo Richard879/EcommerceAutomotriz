f8c64c7063e22434983fbe7ac059b263639988ed
Richard Efio Rivas <richard.efio@gpsinka.com>
2019-04-02 16:32:20 -0500

    Sap Integracion Version 1.0

--- app/Http/Controllers/Api/SapAsientoContableController.php
+++ app/Http/Controllers/Api/SapAsientoContableController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\api;
 use GuzzleHttp\Client;
 use GuzzleHttp\Cookie\CookieJar;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 
