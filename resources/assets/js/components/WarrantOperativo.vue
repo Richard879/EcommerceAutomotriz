45832e0642488dd7b3696c0538808dde1a407d28
Richard Efio Rivas <richard.efio@gpsinka.com>
2019-04-15 10:26:54 -0500

    Sap Integracion Version 1.0

--- resources/assets/js/components/WarrantOperativo.vue
+++ resources/assets/js/components/WarrantOperativo.vue
@@ -1265,29 +1265,22 @@
                     let arrayFR = [];
 
                     me.arraySapRespuesta = response.data;
-                    me.arraySapRespuesta.map(function(x){
-                        arrayFR.push(x);
-                    });
+                    me.arraySapRespuesta.map(function(value, key){
+                        me.arraySapUpdSgc.push({
+                            'cTipo'        : "WO",
+                            'cFlagTipo'    : "FR",
+                            'cItemCode'    : value.cItemCode,
+                            'nDocEntry'    : value.nDocEntry,
+                            'nDocNum'      : value.nDocNum,
+                            'cDocType'     : 'items',
+                            'fDocRate'     : value.fDocRate,
+                            'cDocStatus'   : value.cDocStatus,
+                            'cLogRespuesta': ''
+                        });
 
-                    arrayFR.map(function(value, key){
-                        //Si la Factura de Reserva se encuentra ABIERTA
-                        if(value.cDocStatus == 'O'){
-                            me.arraySapUpdSgc.push({
-                                'cTipo'        : "WO",
-                                'cFlagTipo'    : "FR",
-                                'cItemCode'    : value.cItemCode,
-                                'nDocEntry'    : value.nDocEntry,
-                                'nDocNum'      : value.nDocNum,
-                                'cDocType'     : 'items',
-                                'fDocRate'     : value.fDocRate,
-                                'cDocStatus'   : value.cDocStatus,
-                                'cLogRespuesta': ''
-                            });
-
-                            setTimeout(function() {
-                                me.registraSgcFacturaReserva();
-                            }, 600);
-                        }
+                        setTimeout(function() {
+                            me.registraSgcFacturaReserva();
+                        }, 600);
                     });
                 }).catch(error => {
                     me.limpiarPorError("Error en la Integración Factura Reserva SapB1!");
