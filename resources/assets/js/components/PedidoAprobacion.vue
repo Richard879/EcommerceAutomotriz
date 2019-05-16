8fae95685173930380846569db08322c23bf7d82
Richard Efio Rivas <richard.efio@gpsinka.com>
2019-05-16 17:07:51 -0500

    Sistema Gestion Comercial v1.0

--- resources/assets/js/components/PedidoAprobacion.vue
+++ resources/assets/js/components/PedidoAprobacion.vue
@@ -58,7 +58,7 @@
                                                                     </div>
                                                                 </div>
                                                                 <div class="form-group row">
-                                                                    <div class="col-sm-6">
+                                                                    <!--<div class="col-sm-6">
                                                                         <div class="row">
                                                                             <label class="col-sm-4 form-control-label">* Tipo Persona</label>
                                                                             <div class="col-sm-8">
@@ -68,12 +68,20 @@
                                                                                 </label>
                                                                             </div>
                                                                         </div>
+                                                                    </div>-->
+                                                                    <div class="col-sm-6">
+                                                                        <div class="row">
+                                                                            <label class="col-sm-4 form-control-label">* Contacto</label>
+                                                                            <div class="col-sm-8">
+                                                                                <input type="text" v-model="fillPedido.ccontacto" @keyup.enter="listarPedidos(1)" class="form-control form-control-sm">
+                                                                            </div>
+                                                                        </div>
                                                                     </div>
                                                                     <div class="col-sm-6">
                                                                         <div class="row">
-                                                                            <label class="col-sm-4 form-control-label">Contacto</label>
+                                                                            <label class="col-sm-4 form-control-label">* Nro Documento</label>
                                                                             <div class="col-sm-8">
-                                                                                <input type="text" v-model="fillPedido.ccontacto" @keyup.enter="listarPedidos(1)" class="form-control form-control-sm">
+                                                                                <input type="text" v-model="fillPedido.cnrodocumento" @keyup.enter="listarCarteraMesPorVendedor(1)" class="form-control form-control-sm">
                                                                             </div>
                                                                         </div>
                                                                     </div>
@@ -979,6 +987,7 @@
                     nidestadopedido: '',
                     ntipopersona: 1,
                     ccontacto: '',
+                    cnrodocumento: '',
                     nidmarca: '',
                     nidmodelo: ''
                 },
@@ -1203,6 +1212,7 @@
                         'nidmodelo'         :   this.fillBusquedaPedido.nidmodelo,
                         'nidestadopedido'   :   this.fillPedido.nidestadopedido,
                         'ccontacto'         :   this.fillPedido.ccontacto,
+                        'cnrodocumento'     :   this.fillPedido.cnrodocumento,
                         'ntipopersona'      :   this.fillPedido.ntipopersona,
                         'page' : page
                     }
