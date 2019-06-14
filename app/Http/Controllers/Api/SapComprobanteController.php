816555bb9005d403d03a8e7b3db89ae5fbfda41f
Richard Efio Rivas <richard.efio@gpsinka.com>
2019-06-14 17:14:24 -0500

    Sistema Gestion Comercial v1.0

--- app/Http/Controllers/Api/SapComprobanteController.php
+++ app/Http/Controllers/Api/SapComprobanteController.php
@@ -185,4 +185,56 @@ class SapComprobanteController extends Controller
         }
         return $rptaSap;
     }
+
+    public function SapSetFacturaReservaBorrador(Request $request)
+    {
+        $client = new Client([
+            'verify'    => false,
+            'base_uri'  => 'http://172.20.0.10:8020/'
+        ]);
+
+        $array_rpta = [];
+        $rptaSap   = [];
+        $ReceptionDate  = date('Y-m-d');
+
+        //======= Obtener el EmployeeCode del Usuario Autenticado
+        $data = DB::select('exec [usp_Usuario_GetEmpleadoByUsuario] ?',
+        [   Auth::user()->id
+        ]);
+        $nSalesEmployeeCode   =   $data[0]->nSalesEmployeeCode;
+        //============================================================
+
+
+        $data = $request->data;
+        foreach ($data as $key => $value) {
+
+            $json = [
+                'json' => [
+                    "CardCode"          =>  $request->cCardCode,
+                    "DocDate"           =>  (string)$request->fDocDate,
+                    "DocDueDate"        =>  (string)$request->fDocDueDate,
+                    "DocCurrency"       =>  "US$",
+                    "SalesPersonCode"   =>  (string)$nSalesEmployeeCode,
+                    "DocumentLines"     =>  [
+                        [
+                            "ItemCode"          => (string)$value['cNumeroVin'],
+                            "Quantity"          => "1",
+                            "TaxCode"           => "IGV",
+                            "PriceAfterVAT"     => (string)$value['fTotalCompra'],
+                            "Currency"          => "US$",
+                            "WarehouseCode"     => (string)$request->cWarehouseCode,
+                            "ProjectCode"       => (string)$value['cNumeroVin'],
+                            "CostingCode2"      =>  "01", //UnidadDeNegocio
+                            "CostingCode3"      =>  (string)$request->nIdSapSucursal
+                        ]
+                    ]
+                ]
+            ];
+
+            $response = $client->request('POST', "/pruebas/Comprobante/SapSetFacturaReservaBorrador/", $json);
+            $rptaSap = json_decode($response->getBody());
+            array_push($array_rpta, $rptaSap);
+        }
+        return $array_rpta;
+    }
 }
