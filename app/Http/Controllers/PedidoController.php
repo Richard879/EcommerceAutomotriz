d073c8d4eb3f2fdab459788a13328ffee411a020
Richard Efio Rivas <richard.efio@gpsinka.com>
2019-06-25 11:33:42 -0500

    Sistema Gestion Comercial v1.0

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -405,7 +405,7 @@ class PedidoController extends Controller
                                                                                 Auth::user()->id
                                                                             ]);
 
-        $arrayPedido = ParametroController::arrayPaginator($arrayPedido, $request);
+        //$arrayPedido = ParametroController::arrayPaginator($arrayPedido, $request);
         return ['arrayPedido'=>$arrayPedido];
     }
 
