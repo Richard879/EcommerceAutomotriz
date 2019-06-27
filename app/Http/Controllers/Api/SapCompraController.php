bc55cc002caca524e1756eb772955a02e42ffb88
Richard Efio Rivas <richard.efio@gpsinka.com>
2019-06-27 09:51:15 -0500

    Sistema Gestion Comercial v1.0

--- app/Http/Controllers/Api/SapCompraController.php
+++ app/Http/Controllers/Api/SapCompraController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Api\SapCompraController as SapLogin;
 use GuzzleHttp\Client;
 use GuzzleHttp\Cookie\CookieJar;
 use Illuminate\Http\Request;
